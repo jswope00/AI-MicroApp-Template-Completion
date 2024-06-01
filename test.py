acc0d210f61388ee80e5f715d18d52aee4ec50b9
rajaauditone <57040106+rajaathota72-fc@users.noreply.github.com>
2024-06-01 13:05:59 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from openai import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
-openai_api_key = os.getenv('OPENAI_API_KEY')
-openai_client = OpenAI(api_key=openai_api_key)
-openai_client.organization = "org-mTUrTQANr2hY7R44zvFAPKfE"
-#openai_response = openai_client.chat.completions.create(model= "gpt-4o",messages=[{"role": "user", "content": "hello"}])
-print(openai_client.models.list())
